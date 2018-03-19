e35f39a485140dab27b8af4f1e419a76e4e00010
Deniz Sezen <deniz@denizsezen.com>
2018-03-18 21:53:52 -0400

    Modify vc2017-32bit build file to generate projects for bullet.

--- src_main/cmake-msvc2017-32bit.bat
+++ src_main/cmake-msvc2017-32bit.bat
@@ -1,3 +1,18 @@
+REM build dependencies
+pushd libs
+
+REM -----------------------------------
+REM BULLET 3
+REM -----------------------------------
+cd bullet3
+cmake -DBUILD_PYBULLET=ON -DUSE_DOUBLE_PRECISION=ON -DCMAKE_BUILD_TYPE=Release -DPYTHON_INCLUDE_DIR="C:\Program Files (x86)\Python36-32\include" -DPYTHON_LIBRARY="C:\Program Files (x86)\Python36-32\libs\python35.lib" -DPYTHON_DEBUG_LIBRARY="C:\Program Files (x86)\Python36-32\libs\python35_d.lib" -G "Visual Studio 15" .
+
+REM back to src_main
+popd
+
+REM -----------------------------------
+REM generate projects for main game.
+REM -----------------------------------
 del /s /q build
 mkdir build
 cd build
